4816092b4928bf0b8b0dac7ab524197804337048
Nguyễn Minh Nhật <123147485+NguyenNhat4@users.noreply.github.com>
2025-06-25 21:34:24 +0700

    refactor(routes): rename route imports for clarity and maintainability
    Updates route imports in main.py to use more descriptive names, enhancing code readability. Additionally, introduces a new FeedbackService in services, improving the structure of service management. This change aligns with established coding standards and improves maintainability of the codebase.

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,7 +2,9 @@ from app.routes.user_routes import router as user_controller
 from app.routes.conversation_routes import router as conversation_controller
 from app.routes.audio_routes import router as audio_controller
 from app.routes.message_routes import router as message_controller
+from app.routes.image_description import router as image_description_controller
 from app.routes.tts_routes import router as tts_controller
+
 
 __all__ = [
     'user_controller',
@@ -10,4 +12,5 @@ __all__ = [
     'audio_controller',
     'message_controller',
     'tts_controller',
+    'image_description_controller',
 ]
